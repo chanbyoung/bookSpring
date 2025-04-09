0b63879efb53429eef7200c90b4f91a3ce605b2e
pcb7893@naver.com <122460524+chanbyoung@users.noreply.github.com>
2025-04-09 23:55:16 +0900

    chore: 프로젝트 생성

---
+++ src/main/java/book/bookspring/BookspringApplication.java
@@ -0,0 +1,13 @@
+package book.bookspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookspringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookspringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/book/bookspring/BookspringApplicationTests.java
@@ -0,0 +1,13 @@
+package book.bookspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookspringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
